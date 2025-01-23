796e336d747ca562cfc9c0a977989534dd0e200a
pankaj sharma <2000pks@gmail.com>
2025-01-23 21:29:02 +0530

    added hosts for railway

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS=[]
+ALLOWED_HOSTS = ["storewebsiteprivate-production.up.railway.app","https://storewebsiteprivate-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS=["storewebsiteprivate-production.up.railway.app","https://storewebsiteprivate-production.up.railway.app"]
 
 
 # Application definition
